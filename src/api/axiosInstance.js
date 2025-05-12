ab0d2300cec75de1bcee193919fbeb287ae8cef8
Nikhil <kumarnikhil48578@gmail.com>
2025-05-12 22:49:07 +0530

    update

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://lms-backend-production-dc37.up.railway.app",
 });
 
 axiosInstance.interceptors.request.use(
